1de82f2322ec7f6724528b67125bb8f66108c832
m1lkis <m1lkis.grust.co@gmail.com>
2025-08-23 11:16:05 +0300

    Update rust_jackhammer.lua
    Update rust_jackhammer.lua

--- entities/weapons/rust_jackhammer.lua
+++ entities/weapons/rust_jackhammer.lua
@@ -16,7 +16,7 @@ SWEP.DownPos            = Vector(-0, 0, -3)
 
 
 
-SWEP.Damage             = 1500000
+SWEP.Damage             = 15
 
 
 
@@ -72,9 +72,7 @@ function SWEP:PrimaryAttack()
 
 end
 
-function SWEP:PrimaryAttack()
 
-end
 
 function SWEP:Throw()
 
