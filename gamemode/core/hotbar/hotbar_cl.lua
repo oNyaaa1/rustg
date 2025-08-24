4292f150a14a509c90d2577bb4f731dbc5bb69dd
m1lkis <m1lkis.grust.co@gmail.com>
2025-08-24 18:11:05 +0300

    Update hotbar_cl.lua

--- gamemode/core/hotbar/hotbar_cl.lua
+++ gamemode/core/hotbar/hotbar_cl.lua
@@ -1,67 +1,57 @@
 local SelectedSlot = 0
+local lastSentSlot = 0
 
-local function CheckSlots(desc)
-    if (SelectedSlot == nil or SelectedSlot == 0) then
-        gRust.ResetDeploy()
+local function SendSelect(slot)
+    if slot ~= lastSentSlot then
         net.Start("gRust.SelectWeapon")
-        net.WriteUInt(0, 3)
+        net.WriteUInt(slot, 3)
         net.SendToServer()
-        gRust.Hotbar:SetSelection(nil)
+        lastSentSlot = slot
+    end
+end
+
+local function CheckSlots()
+    if not SelectedSlot or SelectedSlot == 0 then
+        gRust.ResetDeploy()
+        SendSelect(0)
+        if IsValid(gRust.Hotbar) then gRust.Hotbar:SetSelection(nil) end
         return
     end
 
-    local pl = LocalPlayer()
-    if (SelectedSlot > 6) then
-        SelectedSlot = 1
-    elseif (SelectedSlot < 1) then
-        SelectedSlot = 6
-    end
+    if SelectedSlot > 6 then SelectedSlot = 1 end
+    if SelectedSlot < 1 then SelectedSlot = 6 end
 
-    if (!pl.Inventory) then return end
+    local pl = LocalPlayer()
+    if not pl.Inventory then return end
 
     pl.SelectedSlotIndex = SelectedSlot
     pl.SelectedSlot = pl.Inventory[SelectedSlot]
+
     gRust.ResetDeploy()
+    SendSelect(SelectedSlot)
 
-    net.Start("gRust.SelectWeapon")
-    net.WriteUInt(SelectedSlot, 3)
-    net.SendToServer()
+    if IsValid(gRust.Hotbar) then gRust.Hotbar:SetSelection(SelectedSlot) end
 
-    local Item = pl.Inventory[SelectedSlot]
-    if (!Item) then
-        gRust.Hotbar:SetSelection(SelectedSlot)
-        return
-    end
-
-    gRust.Hotbar:SetSelection(SelectedSlot)
-    if (gRust.Items[Item:GetItem()]:GetEntity()) then
+    local item = pl.Inventory[SelectedSlot]
+    if item and gRust.Items[item:GetItem()]:GetEntity() then
         gRust.RequestDeploy(SelectedSlot)
     end
 end
 
-hook.Add("PlayerBindPress", "gRust.SelectSlot", function(pl, bind, pressed, code)
-    if (string.sub(bind, 1, 4) ~= "slot") then return end
-    local slotNum = tonumber(string.sub(bind, 5))
-    if SelectedSlot == 0 or SelectedSlot == nil then
-        SelectedSlot = slotNum
-    else
-        if SelectedSlot == slotNum then
-            SelectedSlot = 0
-        else
-            SelectedSlot = slotNum
-        end 
-    end
+hook.Add("PlayerBindPress", "gRust.SelectSlot", function(_, bind)
+    if string.sub(bind, 1, 4) ~= "slot" then return end
+    local num = tonumber(string.sub(bind, 5))
+    if not num then return end
+    SelectedSlot = (SelectedSlot == num) and 0 or num
     CheckSlots()
 end)
 
-gRust.AddBind("invnext", function(pl)
-    SelectedSlot = SelectedSlot or 0
-    SelectedSlot = SelectedSlot + 1
+gRust.AddBind("invnext", function()
+    SelectedSlot = (SelectedSlot or 0) + 1
     CheckSlots()
 end)
 
-gRust.AddBind("invprev", function(pl)
-    SelectedSlot = SelectedSlot or 2
-    SelectedSlot = SelectedSlot - 1
-    CheckSlots(true)
+gRust.AddBind("invprev", function()
+    SelectedSlot = (SelectedSlot or 7) - 1
+    CheckSlots()
 end)
