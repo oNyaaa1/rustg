e9918d7df8a645122f95de662d19c935563ffc7d
marcandreher <inbox@osu-server-list.com>
2025-08-28 08:59:04 +0000

    ref: junkpile

--- entities/entities/rust_junkpile/init.lua
+++ entities/entities/rust_junkpile/init.lua
@@ -3,7 +3,7 @@ AddCSLuaFile("shared.lua")
 
 include("shared.lua")
 
-local RoadSignModels = {
+local JunkpileModels = {
     "models/environment/junkpiles/b.mdl",
     "models/environment/junkpiles/a.mdl",
     "models/environment/junkpiles/c.mdl",
@@ -17,12 +17,33 @@ function ENT:Initialize()
     if CLIENT then return end
     
     -- Pick a random roadsign model
-    local randomModel = RoadSignModels[math.random(#RoadSignModels)]
+    local randomModel = JunkpileModels[math.random(#JunkpileModels)]
     self:SetModel(randomModel)
     
     self:PhysicsInitStatic(SOLID_VPHYSICS)
     self:SetMoveType(MOVETYPE_NONE)
     self:SetSolid(SOLID_VPHYSICS)
-    
+
     self:PrecacheGibs()
+
+    -- Spawn barrels at max 2 or boxes around it
+    -- rust_barrel.lua
+    for i = 1, math.random(1, 2) do
+        local barrel = ents.Create("rust_barrel")
+        if IsValid(barrel) then
+            barrel:SetPos(self:GetPos() + Vector(math.random(-50, 50), math.random(-50, 50), 0))
+            barrel:Spawn()
+            barrel:DropToFloor()
+        end
+    end
+
+    -- rust_woodencrate.lua
+    for i = 1, math.random(0, 1) do
+        local crate = ents.Create("rust_woodencrate")
+        if IsValid(crate) then
+            crate:SetPos(self:GetPos() + Vector(math.random(-50, 50), math.random(-50, 50), 0))
+            crate:Spawn()
+            crate:DropToFloor()
+        end
+    end
 end
